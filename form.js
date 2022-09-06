169e3baafe8cc1b04b557029f8834186b79831ad
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-09-06 22:29:53 +0530

    form

--- form.js
+++ form.js
@@ -10,27 +10,27 @@ backFun = () => {
     document.getElementById('f_col').style.display = 'none'
     document.getElementById('ff_col').style.display = 'none'
     document.getElementById('bb_btn').disabled = true
-    document.getElementById('nb_btn').disabled = false
-    document.getElementById('nb_btn').style.display = 'flex'
+    document.getElementById('nb_btns').disabled = false
+    document.getElementById('nb_btns').style.display = 'flex'
     document.getElementById('bt_gen').style.display = 'none'
   } else if (cnt == 2) {
     document.getElementById('sc_div').style.display = 'flex'
     document.getElementById('thc_div').style.display = 'none'
     document.getElementById('f_col').style.display = 'none'
-    document.getElementById('nb_btn').style.display = 'flex'
+    document.getElementById('nb_btns').style.display = 'flex'
     document.getElementById('bt_gen').style.display = 'none'
   } else if (cnt == 3) {
     document.getElementById('f_col').style.display = 'none'
     document.getElementById('ff_col').style.display = 'none'
     document.getElementById('thc_div').style.display = 'flex'
-    document.getElementById('nb_btn').style.display = 'flex'
+    document.getElementById('nb_btns').style.display = 'flex'
     document.getElementById('bt_gen').style.display = 'none'
     // document.getElementById('bd-con').style.display = 'none'
   }
   else if (cnt == 4) {
     document.getElementById('f_col').style.display = 'flex'
     document.getElementById('ff_col').style.display = 'none'
-    document.getElementById('nb_btn').style.display = 'none'
+    document.getElementById('nb_btns').style.display = 'none'
     document.getElementById('bt_gen').style.display = 'flex'
   }
   cnt--
@@ -38,27 +38,28 @@ backFun = () => {
 // -----------|-<
 // for-ward button
 // document.getElementById('nb_btn').disabled = true
+document.getElementById('bt_gen').style.display = 'none'
 nextFun = () => {
   if (cnt == 1) {
     document.getElementById('fc_div').style.display = 'none'
     document.getElementById('sc_div').style.display = 'flex'
     document.getElementById('bb_btn').disabled = false
-    document.getElementById('nb_btn').style.display = 'flex'
+    document.getElementById('nb_btns').style.display = 'flex'
   } else if (cnt == 2) {
     document.getElementById('sc_div').style.display = 'none'
     document.getElementById('thc_div').style.display = 'flex'
     document.getElementById('f_col').style.display = 'none'
-    document.getElementById('nb_btn').style.display = 'flex'
-    document.getElementById('bt_gen').style.display = 'none'
+    document.getElementById('nb_btns').style.display = 'flex'
+    // document.getElementById('bt_gen').style.display = 'none'
   } else if (cnt == 3) {
     document.getElementById('thc_div').style.display = 'none'
     document.getElementById('f_col').style.display = 'flex'
-    document.getElementById('nb_btn').style.display = 'flex'
-    document.getElementById('bt_gen').style.display = 'none'
+    document.getElementById('nb_btns').style.display = 'flex'
+    // document.getElementById('bt_gen').style.display = 'none'
   } else if (cnt == 4) {
     document.getElementById('f_col').style.display = 'none'
     document.getElementById('ff_col').style.display = 'flex'
-    document.getElementById('nb_btn').style.display = 'none'
+    document.getElementById('nb_btns').style.display = 'hidden'
     document.getElementById('bt_gen').style.display = 'flex'
     console.log("your nxt btn is display non")
   }
