7d44a1324a176c3219612749a6c13b768dd81d49
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-09-12 23:51:59 +0530

    navbar smooth action for mobile version

--- form.js
+++ form.js
@@ -6,31 +6,23 @@ backFun = () => {
   if (cnt == 1) {
     document.getElementById('fc_div').style.display = 'flex'
     document.getElementById('sc_div').style.display = 'none'
-    document.getElementById('thc_div').style.display = 'none'
-    document.getElementById('f_col').style.display = 'none'
-    document.getElementById('ff_col').style.display = 'none'
     document.getElementById('bb_btn').disabled = true
-    document.getElementById('nb_btns').disabled = false
     document.getElementById('nb_btns').style.display = 'flex'
     document.getElementById('bt_gen').style.display = 'none'
   } else if (cnt == 2) {
     document.getElementById('sc_div').style.display = 'flex'
     document.getElementById('thc_div').style.display = 'none'
-    document.getElementById('f_col').style.display = 'none'
     document.getElementById('nb_btns').style.display = 'flex'
     document.getElementById('bt_gen').style.display = 'none'
   } else if (cnt == 3) {
     document.getElementById('f_col').style.display = 'none'
-    document.getElementById('ff_col').style.display = 'none'
     document.getElementById('thc_div').style.display = 'flex'
     document.getElementById('nb_btns').style.display = 'flex'
     document.getElementById('bt_gen').style.display = 'none'
-    // document.getElementById('bd-con').style.display = 'none'
   }
   else if (cnt == 4) {
     document.getElementById('f_col').style.display = 'flex'
     document.getElementById('ff_col').style.display = 'none'
-    document.getElementById('nb_btns').style.display = 'none'
     document.getElementById('bt_gen').style.display = 'flex'
   }
   cnt--
