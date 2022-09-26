3cbaa4f042d3024d0b29858b3f56813382b6fcc7
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-09-27 00:34:57 +0530

    now the download button is activated

--- resume_01/operation.js
+++ resume_01/operation.js
@@ -162,3 +162,39 @@ themeButton.addEventListener('click', () => {
   localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
+
+/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
+function scaleCv() {
+  document.body.classList.add('scale-cv')
+}
+/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
+function removeScale() {
+  document.body.classList.remove('scale-cv')
+}
+/*==================== GENERATE PDF ====================*/
+// PDF generated area
+let areaCv = document.getElementById('area-cv')
+
+let resumeButton = document.getElementById('resume-button')
+
+// Html2pdf options
+let opt = {
+  margin: 0,
+  filename: 'myResume.pdf',
+  image: { type: 'jpeg', quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: 'a4', orientation: 'portrait' },
+}
+// Function to call areaCv and Html2Pdf options
+function generateResume() {
+  html2pdf(areaCv, opt)
+}
+// When the button is clicked, it executes the three functions
+resumeButton.addEventListener('click', () => {
+  // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+  scaleCv()
+  // 2. The PDF is generated
+  generateResume()
+  // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+  setTimeout(removeScale, 5000)
+})
